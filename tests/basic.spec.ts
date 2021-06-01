1330791b249cb45a691f7ae459af2e215c5aec1a
matzayonc <matzayonc@gmail.com>
2021-06-01 13:09:03 +0200

    added update test

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -1,26 +1,29 @@
 import * as anchor from '@project-serum/anchor';
 
-const myAccount = anchor.web3.Keypair.generate();
-
 
 describe('basic', () => {
 
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
+  const program = anchor.workspace.Basic;
+  const myAccount = anchor.web3.Keypair.generate();
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const program = anchor.workspace.Basic;
-    const myAccount = anchor.web3.Keypair.generate();
-
-    // Atomically create the new account and initialize it with the program.
-    await program.rpc.initialize(new anchor.BN(1234), {
+  it('Initialization', async () => {
+    await program.rpc.initialize(new anchor.BN(42), {
       accounts: {
         user: myAccount.publicKey,
         rent: anchor.web3.SYSVAR_RENT_PUBKEY,
       },
       signers: [myAccount],
       instructions: [await program.account.myAccount.createInstruction(myAccount)],
+    });
+  });
+
+
+  it('Update', async () => {
+    await program.rpc.update(new anchor.BN(44), {
+      accounts: {
+        user: myAccount.publicKey
+      }
     });
   });
 });
