350fef6cf25d87e0460b6758d4ed5f0ead44c0b5
matzayonc <matzayonc@gmail.com>
2021-06-01 12:15:25 +0200

    added data field

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -1,5 +1,8 @@
 import * as anchor from '@project-serum/anchor';
 
+const myAccount = anchor.web3.Keypair.generate();
+
+
 describe('basic', () => {
 
   // Configure the client to use the local cluster.
@@ -8,7 +11,17 @@ describe('basic', () => {
   it('Is initialized!', async () => {
     // Add your test here.
     const program = anchor.workspace.Basic;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
+    const myAccount = anchor.web3.Keypair.generate();
+
+    // Atomically create the new account and initialize it with the program.
+    await program.rpc.initialize(new anchor.BN(1234), {
+      accounts: {
+        myAccount: myAccount.publicKey,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+      },
+      signers: [myAccount],
+      instructions: [await program.account.myAccount.createInstruction(myAccount)],
+    });
+
   });
 });
