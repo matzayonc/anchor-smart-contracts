4494f1ff4167ec23737a61c64c7f0cd1c6e852b6
matzayonc <matzayonc@gmail.com>
2021-06-02 11:44:47 +0200

    counter as state

--- programs/basic/src/lib.rs
+++ programs/basic/src/lib.rs
@@ -4,6 +4,31 @@ use anchor_lang::prelude::*;
 mod manager {
     use super::*;
 
+    #[state]
+    pub struct Counter {
+        pub authority: Pubkey,
+        pub count: u64,
+    }
+
+    impl Counter {
+        pub fn new(ctx: Context<Auth>) -> Result<Self> {
+            Ok(Self {
+                authority: *ctx.accounts.authority.key,
+                count: 0,
+            })
+        }
+
+        pub fn increment(&mut self, ctx: Context<Auth>) -> Result<()> {
+            if &self.authority != ctx.accounts.authority.key {
+                return Err(ErrorCode::Unauthorized.into());
+            } 
+
+            self.count += 1;
+            Ok(())
+        }
+
+    }
+
     pub fn initialize(ctx: Context<InitializeWallet>, data: u64) -> ProgramResult {
         let user = &mut ctx.accounts.user;
         user.tokens = data;
@@ -15,6 +40,7 @@ mod manager {
         user.tokens = data;
         Ok(())
     }
+
 }
 
 #[derive(Accounts)]
@@ -37,7 +63,20 @@ pub struct MyAccount {
 
 
 
+#[error]
+pub enum ErrorCode {
+    #[msg("This is an error message clients will automatically display")]
+    Unauthorized,
+}
 
+#[derive(Accounts)]
+pub struct Auth<'info> {
+    #[account(signer)]
+    authority: AccountInfo<'info>,
+}
+
+
+/*
 mod counter {
     use super::*;
 
@@ -71,3 +110,45 @@ pub struct Update<'info> {
 pub struct Counter {
     pub amount: u64,
 }
+
+
+
+
+*/
+/*
+use anchor_lang::prelude::*;
+
+#[program]
+pub mod basic_4 {
+    use super::*;
+
+    #[state]
+    pub struct Counter {
+        pub authority: Pubkey,
+        pub count: u64,
+    }
+
+    impl Counter {
+        pub fn new(ctx: Context<Auth>) -> Result<Self> {
+            Ok(Self {
+                authority: *ctx.accounts.authority.key,
+                count: 0,
+            })
+        }
+
+        pub fn increment(&mut self, ctx: Context<Auth>) -> Result<()> {
+            if &self.authority != ctx.accounts.authority.key {
+                return Err(ErrorCode::Unauthorized.into());
+            }
+            self.count += 1;
+            Ok(())
+        }
+    }
+}
+
+#[derive(Accounts)]
+pub struct Auth<'info> {
+    #[account(signer)]
+    authority: AccountInfo<'info>,
+}
+*/
--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -1,4 +1,5 @@
 import * as anchor from '@project-serum/anchor';
+import { assert } from 'chai'
 
 
 describe('basic', () => {
@@ -26,4 +27,31 @@ describe('basic', () => {
       }
     });
   });
+
+  it(('CountInit'), async () => {
+    await program.state.rpc.new({
+      accounts: {
+        authority: anchor.Provider.env().wallet.publicKey,
+      },
+    });
+
+    const {count} = await program.state.fetch()
+    assert.ok(count.eq(new anchor.BN(0)))
+  })
+
+  it(('Counter'), async () => {
+
+    let expectedCount = Math.floor(Math.random() * 10+2)
+
+    for(let i = 0; i < expectedCount; i++){
+      await program.state.rpc.increment({
+        accounts: {
+          authority: anchor.Provider.env().wallet.publicKey,
+        },
+      });
+    }
+
+    const {count} = await program.state.fetch()
+    assert.ok(count.eq(new anchor.BN(expectedCount)))
+  })
 });
